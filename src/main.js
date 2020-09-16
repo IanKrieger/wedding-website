64e186bee8801f51530e7fdf0caf58e98830e73c
Ian Krieger <iandkrieger@gmail.com>
2020-09-15 22:00:56 -0400

    Initial vue app commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vuelidate from 'vuelidate'
+import 'es6-promise/auto'
+import Vuex from 'vuex'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import store from './store'
+
+Vue.use(Vuex)
+Vue.use(Vuelidate)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store
 }).$mount('#app')
