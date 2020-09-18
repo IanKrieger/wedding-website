4e20f51ccf3d813c7340ea275d87717a9e124837
Ian Krieger <iandkrieger@gmail.com>
2020-09-17 22:36:13 -0400

    personId?

--- src/constants/const.js
+++ src/constants/const.js
@@ -1,24 +1,24 @@
 const invitee = [
     {
-        id: 1,
+        personId: 1,
         name: "Ian Krieger",
         hasPlusOne: false,
         groupList: ["Ian Krieger"]
     },
     {
-        id: 2,
+        personId: 2,
         name: "Ian One,Another Person",
         hasPlusOne: false,
         groupList: ["Ian Krieger", "Another Person"]
     },
     {
-        id: 3,
+        personId: 3,
         name: "Ian Two,Ian Three,Ian Four,Ian Five",
         hasPlusOne: false,
         groupList: ["Ian Two","Ian Three","Ian Four","Ian Five"]
     },
     {
-        id: 4,
+        personId: 4,
         name: "Ian Six",
         hasPlusOne: true,
         groupList: ["Ian Krieger"]
