ff8a4ccdea529d8f7ce95c853315fcc284eca094
Ian Krieger <iandkrieger@gmail.com>
2020-09-17 21:20:50 -0400

    graphql

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,57 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateAttending = /* GraphQL */ `
+  subscription OnCreateAttending {
+    onCreateAttending {
+      id
+      isAttending
+      submitObject {
+        emailAddress
+        personDetails {
+          name
+          dietaryRestrictions
+          status
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateAttending = /* GraphQL */ `
+  subscription OnUpdateAttending {
+    onUpdateAttending {
+      id
+      isAttending
+      submitObject {
+        emailAddress
+        personDetails {
+          name
+          dietaryRestrictions
+          status
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteAttending = /* GraphQL */ `
+  subscription OnDeleteAttending {
+    onDeleteAttending {
+      id
+      isAttending
+      submitObject {
+        emailAddress
+        personDetails {
+          name
+          dietaryRestrictions
+          status
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
